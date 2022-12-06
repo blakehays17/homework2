9efeb4792011e5f1c450910a4479a4a10632802a
blakehays17 <blakehays@ou.edu>
2022-12-06 02:12:16 -0600

    r

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
-<footer>Designed by Blake Hays &#169;
-    <?php echo date("Y"); ?>
+<footer>
+    <div class="footer">
+        Designed by Blake Hays &#169;
+        <?php echo date("Y"); ?>
+    </div>
 </footer>
